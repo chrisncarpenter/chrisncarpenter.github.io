9c2a5fe45bee389e44c0198285d2e70f219e084f
Chris Carpenter <ccarpenter@irishtimes.com>
2014-08-25 11:30:10 +0100

    Country updates

--- recipe.js
+++ recipe.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	$("h6:contains('USA')").addClass('usa countryOrigin');		
+
+
 	$("h3:contains('Ingredients')").addClass('ingredients');
 	$(".ingredients").next().andSelf().wrapAll('<div id="newDiv" />');
 });
